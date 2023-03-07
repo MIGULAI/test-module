96eeffe4ac5ceb44dd3050bb236d4bf578b89a23
IvanVitiuk <vitukivan05@gmail.com>
2023-03-07 10:44:12 +0200

    added module

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\modules\hello;
+
+use yii\base\BootstrapInterface;
+
+/**
+ * Module module definition class
+ */
+class Module extends \yii\base\Module implements BootstrapInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+     public $controllerNamespace = 'app\modules\hello\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function bootstrap($app)
+    {
+        $app->getUrlManager()->addRules([
+            ['class' => 'yii\web\UrlRule', 'pattern' => $this->id, 'route' => $this->id . '/hello'],
+        ], false);
+    }
+
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here
+
+
+        $this->controllerMap = [
+            'smth' => 'app\modules\hello\controllers\DefaultController',
+        ];
+    }
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\modules\hello\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `Module` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render( '/default/index');
+    }
+    public function actionHellome(){
+        return $this->render('/default/index');
+    }
+}
